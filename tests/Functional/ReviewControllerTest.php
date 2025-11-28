65ed768cf79e7d9f93ae07df205613b259a6b1c7
benoit souillat <dev.souillat@gmail.com>
2025-11-28 13:37:31 +0100

    fixing Repository Call and getFormData in tests

--- tests/Functional/ReviewControllerTest.php
+++ tests/Functional/ReviewControllerTest.php
@@ -4,6 +4,7 @@ namespace App\Tests\Functional;
 
 use App\Model\Entity\Tag;
 use App\Model\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,9 +18,8 @@ class ReviewControllerTest extends WebTestCase
     {
         $this->client = static::createClient();
         $this->client->followRedirects();
-        $this->userRepository = $this->getEntityManager()->getRepository(User::class);
+        $this->userRepository = $this->client->getContainer()->get(EntityManagerInterface::class)->getRepository(User::class);
         $this->tagsRepository = $this->client->getContainer()->get('doctrine.orm.entity_manager')->getRepository(Tag::class);
-
 
         $this->urlGenerator = $this->client->getContainer()->get('router.default');
         $this->user = $this->userRepository->findOneByEmail('user+1@email.com');
