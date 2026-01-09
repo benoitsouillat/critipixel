49007fa37a3c7f0bab91f2b158eeba8c8ce92a2c
benoit souillat <dev.souillat@gmail.com>
2026-01-09 11:36:36 +0100

    update connection test

--- tests/DatabaseConnectionTest.php
+++ tests/DatabaseConnectionTest.php
@@ -2,33 +2,20 @@
 
 declare(strict_types=1);
 
+use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DatabaseConnectionTest extends WebTestCase
 {
-    protected string $databaseHost;
-    protected int $databasePort;
-    protected string $databaseUser;
-    protected string $databasePassword;
-    protected string $databaseName;
-
-    public function setUp(): void
-    {
-        $this->databaseHost = getenv('DB_HOST');
-        $this->databasePort = (int)getenv('DB_PORT');
-        $this->databaseUser = getenv('DB_USER');
-        $this->databasePassword = getenv('DB_PASSWORD');
-        $this->databaseName = getenv('DB_NAME');
-    }
-
     public function testConnection(): void
     {
-        $connection = new \PDO(
-            "postgresql:host={$this->databaseHost};port={$this->databasePort};dbname={$this->databaseName}",
-            $this->databaseUser,
-            $this->databasePassword
-        );
-        $this->assertNotFalse($connection);
+        self::bootKernel();
+
+        $connection = self::getContainer()->get(Connection::class);
+
+        $result = $connection->executeQuery('SELECT 1')->fetchOne();
+
+        $this->assertEquals(1, $result, "La base de données n'a pas répondu correctement.");
     }
 
 }
